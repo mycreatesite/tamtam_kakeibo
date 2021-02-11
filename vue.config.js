c58dabbd6bd9da00f95e77d02c8ae222ac9d4a9e
ma-ya's create <hello.myscreate@gmail.com>
2021-02-11 15:14:39 +0900

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/tamtam_kakeibo' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
